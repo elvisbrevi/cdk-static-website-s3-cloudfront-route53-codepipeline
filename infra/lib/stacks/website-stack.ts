57d14982e209cf1c5ef48d72cb8f23c4a4634b0e
elvisbrevi <elvisbrevi@gmail.com>
2023-05-08 12:21:07 -0400

    added bucketname to parameter store

--- infra/lib/stacks/website-stack.ts
+++ infra/lib/stacks/website-stack.ts
@@ -1,6 +1,6 @@
 "use strict";
 import { Construct } from 'constructs';
-import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Duration, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
 import * as route53 from 'aws-cdk-lib/aws-route53';
 import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
@@ -8,6 +8,7 @@ import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
 import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
 import { CloudFrontTarget } from 'aws-cdk-lib/aws-route53-targets';
 import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as ssm from 'aws-cdk-lib/aws-ssm';
 
 const DOMAIN_NAME = "elvisbrevi.com";
 const WWW_DOMAIN_NAME = `www.${DOMAIN_NAME}`;
@@ -21,8 +22,14 @@ export class WebSiteStack extends Stack {
         const staticWebsiteBucket = new s3.Bucket(this, `WebsiteBucket-${id}`, {
             websiteIndexDocument: 'index.html',
             publicReadAccess: true,
-            removalPolicy: RemovalPolicy.DESTROY,
+            removalPolicy: RemovalPolicy.RETAIN,
         });
+
+        // Create ssm for the bucket name 
+        new ssm.StringParameter(this, `BlogBucketNameParam-${id}`, {
+            parameterName: 'blog-bucket-name',
+            stringValue: staticWebsiteBucket.bucketName
+        }).node.addDependency(staticWebsiteBucket);
 
         // Get the Hosted Zone
         const hostedZone = route53.HostedZone.fromLookup(this, `HostedZone`, { domainName: DOMAIN_NAME });
@@ -50,6 +57,7 @@ export class WebSiteStack extends Stack {
             zone: hostedZone,
             target: route53.RecordTarget.fromAlias(new CloudFrontTarget(cloudFrontDistribution)),
             recordName: DOMAIN_NAME, 
+            //ttl: Duration.seconds(60)
         });
         
         // Same from www.sub-domain
@@ -57,6 +65,7 @@ export class WebSiteStack extends Stack {
             zone: hostedZone,
             target: route53.RecordTarget.fromAlias(new CloudFrontTarget(cloudFrontDistribution)),
             recordName: WWW_DOMAIN_NAME,
+            //ttl: Duration.seconds(60)
         });
 
         // Deploy the website to the hosting bucket
@@ -65,7 +74,11 @@ export class WebSiteStack extends Stack {
             destinationBucket: staticWebsiteBucket,
             distributionPaths: ['/*'], 
             distribution: cloudFrontDistribution,
-            retainOnDelete: false
+            // cacheControl: [
+            //     {
+            //         value: "max-age=1",
+            //     }
+            // ]
         });
         
     }
